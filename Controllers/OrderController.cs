1027405dd116f323641240fb1bb302da2a3a75a4
Marcin Kubicki <marcin.kubicki07054@gmail.com>
2023-04-27 18:07:33 +0200

    Zrobiony punkt 3 wstępnie. Dane wyświetlane z tabeli Order i Client. Podpięcie menu w pliku _Layout. Brakuje informacji o zakupionym produkcie (wyciągnięcie informacji z tabeli Product)

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using KRK_Class7_Task1.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KRK_Class7_Task1.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly MyDbContext _dbContext;
+        public OrderController(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dbContext.Orders.Include(o => o.Client).ToListAsync());
+        }
+    }
+}
